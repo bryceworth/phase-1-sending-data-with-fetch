29a2d627d9646c411e199947244a0e1bfc4ec938
bryceworth <bryceaworth@gmail.com>
2022-04-29 21:39:37 -0700

    commit tree d21dc79bbad04f283a73203f6c6faa63429c5842

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Add your code here
+function submitData(){
+    const formData = {
+    name: "Steve",
+    email: "steve@steve.com",
+  };
+  
+  const configurationObject = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json",
+    },
+    body: JSON.stringify(formData),
+  };
+
+  return fetch("http://localhost:3000/users", configurationObject)
+    .then(function (response) {
+    response.json();
+    })
+    .then(function (object) {
+      console.log(object);
+    })
+    .catch(function (error) {
+        alert("Unauthorized Access");
+        console.log(error.message);
+      });
+} 
